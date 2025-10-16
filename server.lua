eb7f283305cc9ed07b8348d8adcb4bd6aa2d63fd
C <nethukanethsara@hotmail.com>
2025-10-16 21:36:33 +0530

    More enhanced version of the PKG Server
    Now allows you to add more than one config and file, with being able to now make a description about the package with meta.txt

--- server.lua
+++ server.lua
@@ -3,32 +3,50 @@ if not modem or not modem.isWireless() then error("No wireless modem") end
 rednet.open(peripheral.getName(modem))
 
 local packages = {}
-if fs.exists("/PKGServer/packages") then
-    for _, file in ipairs(fs.list("/PKGServer/packages")) do
-        if file:match("%.lua$") then
-            local name = file:gsub("%.lua$", "")
-            local f = fs.open("/PKGServer/packages/"..file, "r")
-            local content = f.readAll()
-            f.close()
-            packages[name] = {["/bin/"..file] = content}
+
+local function loadPackages()
+    if not fs.exists("/PKGServer/packages") then return end
+    for _, pkgName in ipairs(fs.list("/PKGServer/packages")) do
+        local pkgPath = "/PKGServer/packages/" .. pkgName
+        if fs.isDir(pkgPath) then
+            packages[pkgName] = {}
+            if fs.exists(pkgPath.."/files") then
+                for _, file in ipairs(fs.list(pkgPath.."/files")) do
+                    local f = fs.open(pkgPath.."/files/"..file,"r")
+                    local content = f.readAll()
+                    f.close()
+                    packages[pkgName]["/bin/"..file] = content
+                end
+            end
+            if fs.exists(pkgPath.."/configs") then
+                for _, file in ipairs(fs.list(pkgPath.."/configs")) do
+                    local f = fs.open(pkgPath.."/configs/"..file,"r")
+                    local content = f.readAll()
+                    f.close()
+                    packages[pkgName]["/etc/"..pkgName.."/"..file] = content
+                end
+            end
         end
     end
 end
 
+loadPackages()
+
 while true do
     local senderId, message, protocol = rednet.receive()
     if message then
+        local pkg = message.pkg
         if message.type == "PKG_REQUEST" then
-            local pkg = message.pkg
             if packages[pkg] then
                 rednet.send(senderId, {type="PKG_DATA", files=packages[pkg]})
             else
                 rednet.send(senderId, {type="PKG_ERROR", message="Package not found"})
             end
         elseif message.type == "PKG_INFO" then
-            local pkg = message.pkg
             if packages[pkg] then
-                rednet.send(senderId, {type="PKG_INFO_DATA", meta="Package " .. pkg})
+                local meta = {}
+                for path,_ in pairs(packages[pkg]) do table.insert(meta, path) end
+                rednet.send(senderId, {type="PKG_INFO_DATA", meta=table.concat(meta,", ")})
             else
                 rednet.send(senderId, {type="PKG_ERROR", message="Package not found"})
             end
